e66ff7134da3a0c20caf4b828fe9d845fe054aa4
Shane <68620483+shanebotclub@users.noreply.github.com>
2025-02-21 21:11:28 +0000

    Add files via upload

--- src/scripts/just_sonar.py
+++ src/scripts/just_sonar.py
@@ -3,7 +3,7 @@
 import rospy
 from sensor_msgs.msg import Range
 
-sonarLM = 0.0
+#sonarLM = 0.0
 
 
 
@@ -20,7 +20,7 @@ def main():
     rospy.init_node('just_sonar')
 
     rospy.Subscriber("snr_2", Range, sonar_callback)
-
+    #sonarLM = rospy.Subscriber("snr_2", Range, sonar_callback)
     #print("Sonar distance: ", sonarLM)
 
     rospy.spin()
