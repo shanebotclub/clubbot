6064be2f75c24bf401138b48c9a45e2438c8e976
Shane <68620483+shanebotclub@users.noreply.github.com>
2025-06-08 16:49:35 +0100

    Add files via upload

--- src/scripts/move_stop.py
+++ src/scripts/move_stop.py
@@ -3,11 +3,11 @@
 import rospy
 from geometry_msgs.msg import Twist
 
-linear_speed = 0.3  # Set the linear speed for the robot
-angular_speed = 0.0  # Set the angular speed for the robot
+#linear_speed = 0.3  # Set the linear speed for the robot
+#angular_speed = 0.0  # Set the angular speed for the robot
 
 def drive_robot(linear_speed, angular_speed):
-    velocity = Twist
+    velocity = Twist()
     velocity.linear.x = linear_speed
     velocity.angular.z = angular_speed
     return velocity
@@ -18,7 +18,7 @@ def main():
     rospy.init_node('move_stop')
     
     # Create a publisher to send velocity commands to the robot
-    drive = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    drive = rospy.Publisher('cmd_vel', Twist, queue_size=1)
     
     # Set the rate at which to publish messages
     rate = rospy.Rate(1)
@@ -26,7 +26,7 @@ def main():
     while not rospy.is_shutdown():
         # Get the velocity command for driving the robot
        
-        velocity = drive_robot(linear_speed, angular_speed)
+        velocity = drive_robot(0.3, 0.0)
         print("moveing forward")
         
         # Publish the velocity command
@@ -35,11 +35,11 @@ def main():
         # Sleep to maintain the loop rate
         rate.sleep()
 
-        linear_speed = 0.0  # Stop the robot
-        angular_speed = 0.0  # Stop the robot
-        velocity = drive_robot(linear_speed, angular_speed)
+        
+        velocity = drive_robot(0.0, 0.0)
         drive.publish(velocity)  # Publish the stop command
         print("robot stopped")
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
