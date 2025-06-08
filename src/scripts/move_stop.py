57e51cc12311796d120709f0c6c46d7033c79768
Shane <68620483+shanebotclub@users.noreply.github.com>
2025-06-08 17:41:39 +0100

    Add files via upload

--- src/scripts/move_stop.py
+++ src/scripts/move_stop.py
@@ -2,9 +2,19 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
 
-#linear_speed = 0.3  # Set the linear speed for the robot
-#angular_speed = 0.0  # Set the angular speed for the robot
+move = True
+
+LF_bumper = False
+MF_bumper = False
+RF_bumper = False
+
+LB_bumper = False
+MB_bumper = False
+RB_bumper = False
+
+
 
 def drive_robot(linear_speed, angular_speed):
     velocity = Twist()
@@ -12,38 +22,99 @@ def drive_robot(linear_speed, angular_speed):
     velocity.angular.z = angular_speed
     return velocity
 
+# define callback functions for each bumper
+def LF_bumper_callback(data):
+    global LF_bumper
+    LF_bumper = data.data
+
+def MF_bumper_callback(data):
+    global MF_bumper
+    MF_bumper = data.data
+
+def RF_bumper_callback(data):
+    global RF_bumper
+    RF_bumper = data.data
+
+def LB_bumper_callback(data):
+    global LB_bumper
+    LB_bumper = data.data
+
+def MB_bumper_callback(data):
+    global MB_bumper
+    MB_bumper = data.data
+
+def RB_bumper_callback(data):
+    global RB_bumper
+    RB_bumper = data.data
+
+
+# define functions to subscribe to each bumper sensor
+def left_front_bumper():
+    rospy.Subscriber("bpr_lf", Bool, LF_bumper_callback)
+
+def middle_front_bumper():
+    rospy.Subscriber("bpr_mf", Bool, MF_bumper_callback)
+
+def right_front_bumper():
+    rospy.Subscriber("bpr_rf", Bool, RF_bumper_callback)
+
+def left_back_bumper():
+    rospy.Subscriber("bpr_lb", Bool, LB_bumper_callback)
+
+def middle_back_bumper():
+    rospy.Subscriber("bpr_mb", Bool, MB_bumper_callback)
+
+def right_back_bumper():
+    rospy.Subscriber("bpr_rb", Bool, RB_bumper_callback)
+
+
+
 def main():
-    global linear_speed, angular_speed
+    global linear_speed, angular_speed, move
+    
     # Initialize the ROS node
     rospy.init_node('move_stop')
     
     # Create a publisher to send velocity commands to the robot
     drive = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
+    left_front_bumper()
+    middle_front_bumper()
+    right_front_bumper()
+    left_back_bumper()
+    middle_back_bumper()
+    right_back_bumper()
     
     # Set the rate at which to publish messages
     rate = rospy.Rate(1)
     
     while not rospy.is_shutdown():
+
+        if LF_bumper or MF_bumper or RF_bumper or LB_bumper or MB_bumper or RB_bumper:
+            print("bumper pressed, stopping robot")
+            move = False
+
+        if move == False:
+            print("robot stopped")
+            velocity = drive_robot(0.0, 0.0)
+            drive.publish(velocity)
+            rate.sleep()
+            
         # Get the velocity command for driving the robot
        
-        velocity = drive_robot(0.3, 0.0)
-        print("moveing forward")
+        if move:
+            # If no bumpers are pressed, move forward
+            velocity = drive_robot(0.3, 0.0)
+            print("moveing forward")
+            
+            # Publish the velocity command
+            drive.publish(velocity)
         
-        # Publish the velocity command
-        drive.publish(velocity)
-        
-        # Sleep to maintain the loop rate
-        rate.sleep()
+       
 
         
-        velocity = drive_robot(0.0, 0.0)
-        drive.publish(velocity)  # Publish the stop command
-        print("robot stopped")
-        rate.sleep()
+        
     
-    velocity = drive_robot(0.0, 0.0)  # Stop the robot
-    drive.publish(velocity)  # Publish the stop command
-    print("rospy shut down robot stopped")
 
 if __name__ == '__main__':
     try:
