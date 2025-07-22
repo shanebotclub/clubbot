df0513b9f73033ea7511fca799380b36516cc722
Shane <68620483+shanebotclub@users.noreply.github.com>
2025-07-22 16:25:01 +0100

    Add files via upload
    corrected Pin Errors

--- src/scripts/button_state_publisher.py
+++ src/scripts/button_state_publisher.py
@@ -5,9 +5,9 @@ from std_msgs.msg import Bool
 import RPi.GPIO as GPIO
 
 GREEN_BUTTON_GPIO = 6
-BLUE_BUTTON_GPIO = 19
-YELLOW_BUTTON_GPIO = 26
-RED_BUTTON_GPIO = 4
+BLUE_BUTTON_GPIO = 5
+YELLOW_BUTTON_GPIO = 15
+#RED_BUTTON_GPIO = 3
 
 if __name__ == '__main__':
     rospy.init_node('button_state_publisher')
@@ -15,13 +15,13 @@ if __name__ == '__main__':
     g_pub = rospy.Publisher('g_button_state',Bool, queue_size = 10)
     b_pub = rospy.Publisher('b_button_state',Bool, queue_size = 10)
     y_pub = rospy.Publisher('y_button_state',Bool, queue_size = 10)
-    r_pub = rospy.Publisher('r_button_state',Bool, queue_size = 10)
+    #r_pub = rospy.Publisher('r_button_state',Bool, queue_size = 10)
     
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(GREEN_BUTTON_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)
     GPIO.setup(BLUE_BUTTON_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
     GPIO.setup(YELLOW_BUTTON_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-    GPIO.setup(RED_BUTTON_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
+    #GPIO.setup(RED_BUTTON_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
 
     rate = rospy.Rate(10)
 
@@ -29,13 +29,13 @@ if __name__ == '__main__':
         green_state = not GPIO.input(GREEN_BUTTON_GPIO)
         blue_state = not GPIO.input(BLUE_BUTTON_GPIO)
         yellow_state = not GPIO.input(YELLOW_BUTTON_GPIO)
-        red_state = not GPIO.input(RED_BUTTON_GPIO)
+        #red_state = not GPIO.input(RED_BUTTON_GPIO)
         
         
         g_pub.publish(green_state)
         b_pub.publish(blue_state)
         y_pub.publish(yellow_state)
-        r_pub.publish(red_state)
+        #r_pub.publish(red_state)
         
         if green_state == 1:
             print("green button true")
@@ -46,8 +46,8 @@ if __name__ == '__main__':
         if yellow_state == 1:
             print("yellow button true")
 
-        if red_state == 1:
-            print("red button true")
+        #if red_state == 1:
+            #print("red button true")
         
         rate.sleep()
 
